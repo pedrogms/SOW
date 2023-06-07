2f73792e80c0ce410ef2d35c2632197076b8f05b
Pedro Silva <63201456+pedrogms@users.noreply.github.com>
2023-06-07 18:31:53 -0300

    Update 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,33 @@
 window.addEventListener('scroll', function() {
-    var navbar = document.getElementById('navbar');
-    if (window.scrollY > 10) {
-      navbar.classList.add('scrolled');
-    } else {
-      navbar.classList.remove('scrolled');
-    }
-  });
-
-  function toggleNavbar() {
-    var navbar = document.querySelector('.navbar');
-    navbar.classList.toggle('hamburger-active');
+  var navbar = document.getElementById('navbar');
+  if (window.scrollY > 100) {
+    navbar.classList.add('scrolled');
+    navbar.classList.add('bg-dark');
+  } else {
+    navbar.classList.remove('scrolled');
+    navbar.classList.remove('bg-dark');
   }
+});
+
+function toggleNavbar() {
+  var navbar = document.querySelector('.navbar');
+  navbar.classList.toggle('hamburger-active');
+}
+
+
+// scroll dos cards
+
+window.addEventListener('scroll', function() {
+  var elements = document.getElementsByClassName('col-md-6');
   
+  Array.from(elements).forEach(function(element) {
+    var rect = element.getBoundingClientRect();
+    var isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
 
-  // scroll dos cards
-
-  window.addEventListener('scroll', function() {
-    if (window.scrollY > 50) {
-      var element = document.getElementById('sow_conecta');
-      element.style.opacity = '0';
-      
-      // Adicionar classe CSS gradualmente
-      var scrollProgress = (window.scrollY - 10) / (window.innerHeight - 10);
-      var opacityValue = Math.min(scrollProgress, 1);
-      element.style.opacity = opacityValue;
+    if (isVisible) {
+      element.classList.add('reverse-animation');
+    } else {
+      element.classList.remove('reverse-animation');
     }
   });
+});
